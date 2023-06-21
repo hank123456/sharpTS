9c6078734e307610d8f2faa40df2ff2b07690b90
sx <sx@DESKTOP-SX>
2023-06-21 20:04:19 +0800

    添加项目文件。

---
+++ sharpTS.core/sharpTS.core/TestInterface.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+using NLog;
+using Microsoft.Win32.SafeHandles;
+using System.Threading;
+using System.Diagnostics;
+
+namespace sharpTS.core
+{
+    public partial class TestInterface : IDisposable
+    {
+        private bool _disposed;
+        private bool _dutIsInitialized;
+        private bool _swIsInitialized;
+        private string _confFile;
+
+        private static Logger _logger;
+        // Instantiate a SafeHandle instance.
+        private readonly SafeHandle _handle = new SafeFileHandle(IntPtr.Zero, true);
+
+        /// <summary>
+        /// Initializes the test system. Reads configurations, sets logging, initializes instruments etc.
+        /// </summary>
+        /// <param name="confFile">The configuration file </param>
+        /// <param name="logLevel">The logging level</param>
+        public void Initialize(string confFile, int logLevel)
+        {
+            try
+            {
+                if (_swIsInitialized) throw new ApplicationException("Software is already initialized!");
+
+                Thread.CurrentThread.CurrentCulture     = System.Globalization.CultureInfo.InvariantCulture;
+                Thread.CurrentThread.CurrentUICulture   = System.Globalization.CultureInfo.InvariantCulture;
+
+                InitializeLogging();
+
+                var sw = Stopwatch.StartNew();
+
+                _logger.Log(LogLevel.Info, "Initializing using configuration file: " + confFile);
+                _logger.Log(LogLevel.Info, "Starting initializing......");
+
+                _confFile = confFile;
+
+            }catch(Exception ex)
+            {
+                
+            }
+            
+        }
+        private void InitializeLogging()
+        {
+            if (null == LogManager.Configuration) // if cant find config file, use below config.
+            {
+                var config = new NLog.Config.LoggingConfiguration();
+                var logfile = new NLog.Targets.FileTarget() 
+                { 
+                    FileName = "", 
+                    Name = "logfile" 
+                };
+                var logconsole = new NLog.Targets.ConsoleTarget() 
+                { 
+                    Name = "logconsole" 
+                };
+
+                config.LoggingRules.Add(new NLog.Config.LoggingRule("*", LogLevel.Info, logconsole));
+                config.LoggingRules.Add(new NLog.Config.LoggingRule("*", LogLevel.Info, logfile));
+
+                LogManager.Configuration = config;//active
+            }
+            _logger = LogManager.GetCurrentClassLogger();
+            _logger.Info("Logging initialized!");
+        }
+
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (_disposed) return;
+            if (disposing)
+            {
+                _handle.Dispose();
+                //
+                
+            }
+            _disposed = true;
+        }
+    }
+}
---
+++ sharpTS.core/sharpTS.core/TestInterfaceInternal.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+using System.Xml.Linq;
+using System.Xml.XPath;
+using NLog;
+
+namespace sharpTS.core
+{
+    public partial class TestInterface
+    {
+        private readonly Dictionary<string,AssemblyInfo>
+
+
+        protected virtual XElement LoadConfFile()
+        {
+            var mainElement = XDocument.Load(_confFile).Root;
+            if (null == mainElement)
+                throw new ApplicationException("Error in loading configuration file: " + _confFile);
+
+            return mainElement;
+        }
+
+
+
+
+        private struct AssemblyInfo
+        {
+            public Assembly AssyPointer;
+            public IApplicationControl AppCtrl;
+            public bool Initialize { get; set; }
+            public bool Terminate { get; set; }
+            public bool InitializeDut { get; set; }
+            public bool UninitializeDut { get; set; }
+        }
+    }
+}
